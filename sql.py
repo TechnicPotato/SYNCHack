80590220726377e40033c3a0130bd1ba0bf7d5fd
Bill <ZhuangBill@users.noreply.github.com>
2020-08-29 20:00:06 +1000

    Added SQL integration

---
+++ sql.py
@@ -0,0 +1,64 @@
+import sqlite3
+import time
+import opportunity
+
+class SQLDB():
+    """SQL Database for storing information grabbed"""
+    def __init__(self, database_arg=":memory:"):
+        self.conn = sqlite3.connect("db.sqllite")
+        self.cur = self.conn.cursor()
+        self.opID = 0
+    
+    def execute(self, sql_string):
+        """ 
+        Handler to allow multiple commands in a statement
+        """
+        out = None
+        for string in sql_string.split(";"):
+            try:
+                out = self.cur.execute(string)
+            except:
+                pass
+        return out
+
+    # Commit changes to the database
+    def commit(self):
+        self.conn.commit()
+    
+    def db_setup(self):
+        self.cur.execute("""CREATE TABLE Opportunities(
+            Id INT,
+            company TEXT,
+            type TEXT,
+            location TEXT,
+            application_link TEXT,
+            info_link TEXT,
+            info TEXT,
+            review_score FLOAT,
+            review_link TEXT,
+            openD TEXT,
+            closeD TEXT,
+            startD TEXT,
+            minsal INT,
+            maxsal INT,
+            vacancies INT
+        )""")
+        self.conn.commit()
+    #-----------------------------------
+    # Entry addition
+    #-----------------------------------
+    def add_opportunity(self, opportunity: opportunity):
+        """ Adds an opportunity into the DB."""
+        sql_cmd = """
+                INSERT INTO Opportunities
+                VALUES ({id}, {company}, {type}, {location}, {alink}, {ilink}, {info}, {rscore}, {rlink}, {opend}, {closed}, {startd}, {minsal}, {maxsal}, {vac})
+                """
+        sql_cmd = sql_cmd.format(id=self.opID, company=opportunity.company, type=opportunity.o_type, location=opportunity.location,alink=opportunity.application_link,ilink=opportunity.info_link,info=opportunity.info, rscore=opportunity.review_score, rlink=opportunity.link,opend=opportunity.openD, closed=opportunity.closeD,startd=opportunity.start, minsal=opportunity.min, maxsal=opportunity.max, vac=opportunity.vacancies)
+        self.cur.execute(sql_cmd)
+        self.conn.commit()
+        self.opID += 1
+    
+    def test(self):
+        self.cur.execute("""SELECT * FROM Opportunities""")
+        all_rows = self.cur.fetchall()
+        return all_rows
